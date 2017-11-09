cc6c8245d6df40447106594028fea2f12634ba99
leehyohyeon <33116827+leehyohyeon@users.noreply.github.com>
2017-11-09 15:31:11 +0900

    Update tokenizer.py

--- week9/tokenizer.py
+++ week9/tokenizer.py
@@ -1,23 +1,12 @@
 from nltk.tokenize import sent_tokenize, word_tokenize, WordPunctTokenizer
  
- 
- 
 input_text = "Do you know how tokenization works? Since it's my first time to study tokenization it's sometimes hard to understand but actually really awesome! Lets analyze a couple of sentences and figure it out." 
  
- 
- 
 print("\nSentence Tokenizer:")
- 
 print(sent_tokenize(input_text))
  
- 
- 
-print("\nWord tokenizer : ")
- 
+print("\nWord tokenizer : ") 
 print(word_tokenize(input_text))
  
- 
- 
 print("\nWord Punct Tokenizer : ")
- 
 print(WordPunctTokenizer().tokenize(input_text)) 
